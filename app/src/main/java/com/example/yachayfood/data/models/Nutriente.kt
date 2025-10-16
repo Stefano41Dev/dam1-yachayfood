6bda3bbe794422b118f45c2a2b0de7975066b8b8
Stefano <stefano.41.dev@gmail.com>
2025-10-15 20:10:13 -0500

    creacion de las clases

---
+++ app/src/main/java/com/example/yachayfood/data/models/Nutriente.kt
@@ -0,0 +1,14 @@
+package com.example.yachayfood.data.models
+
+data class Nutriente (
+    val carbohidratos: Double,
+    val energia: Int,
+    val grasas: Int,
+    val grasasSaturadas: Double,
+    val hidratosCarbono: Int,
+    val azucares: Int,
+    val fibrasAlimentarias: Int,
+    val proteinas: Double,
+    val calcio: Int,
+    val porcentajeAceitesVegetales : Double
+)
--- app/src/main/java/com/example/yachayfood/data/models/Producto.kt
+++ app/src/main/java/com/example/yachayfood/data/models/Producto.kt
@@ -1,4 +1,17 @@
 package com.example.yachayfood.data.models
 
-class Producto {
-}
+data class Producto (
+    val codigoProducto: String,
+    val nombreProducto: String,
+    val descripcion: String,
+    val clasificacion: Char,
+    val categorias: MutableList<String> = mutableListOf<String>(),
+    val cantidad: Double,
+    val empaquetado: String,
+    val paises: String,
+    val ingredientes: MutableList<String> = mutableListOf<String>(),
+    val imagenUrl: String,
+    val marcas: String,
+    val pais: String,
+    val nutrientes : Nutriente
+)
